da23b143428b8c61316aaf6f75658977bbb7cab2
Dursun Köktürk <dursunserefkokturk@gmail.com>
2024-09-06 13:13:38 +0300

    list-photos-according-to-the-search-made

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,20 +1,14 @@
-// Api Icindeki Image lerin Bulundugu 
-// Degisken Adini Parametre Olarak Veriyoruz
 function ImageItem({ image }) {
-  console.log(image);
-  return (
-    <div>
-      <img
-        className="imageListImg"
-
-        // Image Bilgilerinin Small Halini Aliyoruz
-        src={image.urls.small}
-
-        // Image Bilgilerinin Aciklamalarini Aliyoruz
-        alt={image.alt_description}
-      />
-    </div>
-  );
+    console.log(image);
+    return (
+        <div>
+            <img
+              className="imageListImg"
+              src={image.urls.small}
+              alt={image.alt_description}
+            />
+        </div>
+    );
 }
 
 export default ImageItem;
